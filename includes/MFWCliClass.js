516e1cad98425bedc42ef58dc7472f45a3a51a65
Gor Martsen <Gor.martsen@gmail.com>
2017-04-17 23:14:30 -0400

    Clean up. setup and Install implemented

--- includes/MFWCliClass.js
+++ includes/MFWCliClass.js
@@ -2,16 +2,25 @@
 
 const EventEmitter = require('events').EventEmitter;
 const util = require('util');
-const fs = require('fs');
+const fs = require('fs-extra');
 const Message = require('../includes/message.js');
+const tmp = require('tmp');
+const exec = require('child_process').exec;
 
+/**
+ * Constructor.
+ */
 function MFWCliClass(RootDirectory) {
   EventEmitter.call(this);
   var self = this;
 
 }
-util.inherits(RootDirectory, EventEmitter);
+util.inherits(MFWCliClass, EventEmitter);
 
+/**
+ * Setup method.
+ *   Prepare root directory.
+ */
 MFWCliClass.prototype.setup = function(RootDirectory) {
   var self = this;
   self.RootDirectory = RootDirectory;
@@ -22,6 +31,48 @@ MFWCliClass.prototype.setup = function(RootDirectory) {
   self.checkRootDirectory();
 }
 
+/**
+ * Install method.
+ *   Install service to ROOTDIR/services/SERVICE_NAME directory.
+ */
+MFWCliClass.prototype.install = function(RootDirectory, module) {
+  var self = this;
+  self.RootDirectory = RootDirectory;
+
+  var nameArray = module.split('/');
+  var shortName = nameArray.pop();
+  self.module = {
+    full: module,
+    short: shortName,
+    installDir: self.RootDirectory + '/services/' + shortName
+  }
+
+  self.on('isModuleExists', self.isModuleExists);
+  self.on('isModuleDownloaded', self.isModuleDownloaded);
+
+  self.checkModule(self.module);
+}
+
+/**
+ * Get executed when Root directory get checked for existance.
+ */
+MFWCliClass.prototype.isModuleExists = function(err, type, module) {
+  var self = this;
+  if (err) {
+    return Message.error(err.message);
+  }
+
+  if (type) {
+    Message.warning(module.installDir + ' already exists.');
+    return;
+  }
+  module.tmpDir = tmp.dirSync();
+  self.downloadPackage(module);
+}
+
+/**
+ * Get executed when Root directory get checked for existance.
+ */
 MFWCliClass.prototype.isRootExists = function(err, type) {
   var self = this;
   if (err) {
@@ -41,6 +92,9 @@ MFWCliClass.prototype.isRootExists = function(err, type) {
   });
 }
 
+/**
+ * Get executed when Sub directory get checked for existance.
+ */
 MFWCliClass.prototype.isDirExists = function(err, type, dir) {
   var self = this;
   if (err) {
@@ -58,6 +112,27 @@ MFWCliClass.prototype.isDirExists = function(err, type, dir) {
   });
 }
 
+
+/**
+ * Get executed when Module installed.
+ */
+MFWCliClass.prototype.isModuleDownloaded = function(err, module) {
+  var self = this;
+  if (err) {
+    return Message.error(err.message);
+  }
+  process.chdir(module.installDir);
+  return exec('npm install', function(error, stdout, stderr) {
+    if(error) {
+      return Message.error(module.full + ' installed, but `npm install` failed:' + error.message);
+    }
+    return Message.ok(module.full + ' installed.');
+  });
+
+}
+/**
+ * Process Root directory check.
+ */
 MFWCliClass.prototype.checkRootDirectory = function() {
   var self = this;
   fs.stat(self.RootDirectory, function(err, stats) {
@@ -72,6 +147,9 @@ MFWCliClass.prototype.checkRootDirectory = function() {
   });
 }
 
+/**
+ * Request subdirs check.
+ */
 MFWCliClass.prototype.checkSkel = function() {
   var self = this;
   self.checkDirectory('services');
@@ -80,10 +158,29 @@ MFWCliClass.prototype.checkSkel = function() {
   self.checkDirectory('configs');
 }
 
+/**
+ * Process Sub directory check.
+ */
+MFWCliClass.prototype.checkModule = function(module) {
+  var self = this;
+  fs.stat(module.installDir, function(err, stats) {
+    if (err) {
+      return self.emit('isModuleExists', null, false, module);
+    }
+    if (!stats.isDirectory()) {
+      return self.emit('isModuleExists', new Error(dir + ' is not a directory!'), false, module);
+    }
+    self.emit('isModuleExists', null, true, module);
+  });
+}
+
+/**
+ * Process Sub directory check.
+ */
 MFWCliClass.prototype.checkDirectory = function(subDir) {
   var self = this;
   var Directory = self.RootDirectory + '/' + subDir;
-  fs.stat(self.RootDirectory, function(err, stats) {
+  fs.stat(Directory, function(err, stats) {
     if (err) {
       return self.emit('isDirExists', null, false, Directory);
     }
@@ -91,6 +188,26 @@ MFWCliClass.prototype.checkDirectory = function(subDir) {
       return self.emit('isDirExists', new Error(dir + ' is not a directory!'), false, Directory);
     }
     self.emit('isDirExists', null, true, Directory);
+  });
+}
+
+
+/**
+ * Download Package.
+ */
+MFWCliClass.prototype.downloadPackage = function(module) {
+  var self = this;
+  console.log(module);
+  process.chdir(module.tmpDir.name);
+  exec('npm pack ' + module.full + '|xargs tar -xzpf', function(err, stdout, stderr) {
+    if (err) {
+      return self.emit('isModuleDownloaded', err, module);
+    }
+    fs.move(module.tmpDir.name + '/package/', module.installDir, { overwrite: false }, function(err) {
+      fs.emptyDirSync(module.tmpDir.name);
+      module.tmpDir.removeCallback();
+      return self.emit('isModuleDownloaded', err, module);
+    });
   });
 }
 
