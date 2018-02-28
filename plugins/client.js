c64964e04caff5dbecdc6a0531bc69344b0231c3
Gor Martsen <Gor.martsen@gmail.com>
2018-02-28 12:53:12 -0500

    Codestyle clean up

--- plugins/client.js
+++ plugins/client.js
@@ -8,10 +8,10 @@ const CommonFunc = require('../includes/common.js');
 class ClientClass extends MFWCommandPrototypeClass {
   constructor(settings) {
     super(settings);
-    if(settings.token) {
+    if (settings.token) {
       this.token = settings.token;
     }
-    if(settings.accesstoken) {
+    if (settings.accesstoken) {
       this.accesstoken = settings.accesstoken;
     }
   }
@@ -21,7 +21,7 @@ class ClientClass extends MFWCommandPrototypeClass {
    * @param {string} module - service name. Example: @microservice-framework/microservice-router
    * @param {string} data - not parsed json data.
    */
-  post( module, data ) {
+  post(module, data) {
     if (!this.validateRootDir()) {
       return this.message('error', 'POST Failed');
     }
@@ -36,17 +36,17 @@ class ClientClass extends MFWCommandPrototypeClass {
         }
 
         let client = this.prepareClient(module);
-        if(client === false) {
+        if (client === false) {
           return;
         }
 
-        try{
+        try {
           let jsonData = JSON.parse(data);
           client.post(jsonData, (err, handlerResponse) => {
             this.answerHandler(err, handlerResponse);
           });
         } catch (e) {
-          this.message('error', e.message); 
+          this.message('error', e.message);
         }
       });
     });
@@ -58,7 +58,7 @@ class ClientClass extends MFWCommandPrototypeClass {
    * @param {string} module - service name. Example: @microservice-framework/microservice-router
    * @param {string} id - resource id
    */
-  get( module, id ) {
+  get(module, id) {
     if (!this.validateRootDir()) {
       return this.message('error', 'GET Failed');
     }
@@ -73,10 +73,10 @@ class ClientClass extends MFWCommandPrototypeClass {
         }
 
         let client = this.prepareClient(module);
-        if(client === false) {
+        if (client === false) {
           return;
         }
-        if(this.token) {
+        if (this.token) {
           client.get(id, this.token, (err, handlerResponse) => {
             this.answerHandler(err, handlerResponse);
           });
@@ -96,7 +96,7 @@ class ClientClass extends MFWCommandPrototypeClass {
    * @param {string} id - resource id
    * @param {string} data - not parsed json data.
    */
-  put( module, id, data ) {
+  put(module, id, data) {
     if (!this.validateRootDir()) {
       return this.message('error', 'PUT Failed');
     }
@@ -111,13 +111,13 @@ class ClientClass extends MFWCommandPrototypeClass {
         }
 
         let client = this.prepareClient(module);
-        if(client === false) {
+        if (client === false) {
           return;
         }
 
-        try{
+        try {
           let jsonData = JSON.parse(data);
-          if(this.token) {
+          if (this.token) {
             client.put(id, this.token, jsonData, (err, handlerResponse) => {
               this.answerHandler(err, handlerResponse);
             });
@@ -127,7 +127,7 @@ class ClientClass extends MFWCommandPrototypeClass {
             });
           }
         } catch (e) {
-          this.message('error', e.message); 
+          this.message('error', e.message);
         }
       });
     });
@@ -139,7 +139,7 @@ class ClientClass extends MFWCommandPrototypeClass {
    * @param {string} module - service name. Example: @microservice-framework/microservice-router
    * @param {string} id - resource id
    */
-  delete( module, id ) {
+  delete(module, id) {
     if (!this.validateRootDir()) {
       return this.message('error', 'DELETE Failed');
     }
@@ -154,10 +154,10 @@ class ClientClass extends MFWCommandPrototypeClass {
         }
 
         let client = this.prepareClient(module);
-        if(client === false) {
+        if (client === false) {
           return;
         }
-        if(this.token) {
+        if (this.token) {
           client.delete(id, this.token, (err, handlerResponse) => {
             this.answerHandler(err, handlerResponse);
           });
@@ -176,7 +176,7 @@ class ClientClass extends MFWCommandPrototypeClass {
    * @param {string} module - service name. Example: @microservice-framework/microservice-router
    * @param {string} data - not parsed json data.
    */
-  search( module, data ) {
+  search(module, data) {
     if (!this.validateRootDir()) {
       return this.message('error', 'POST Failed');
     }
@@ -191,17 +191,17 @@ class ClientClass extends MFWCommandPrototypeClass {
         }
 
         let client = this.prepareClient(module);
-        if(client === false) {
+        if (client === false) {
           return;
         }
 
-        try{
+        try {
           let jsonData = JSON.parse(data);
           client.search(jsonData, (err, handlerResponse) => {
             this.answerHandler(err, handlerResponse);
           });
         } catch (e) {
-          this.message('error', e.message); 
+          this.message('error', e.message);
         }
       });
     });
@@ -219,13 +219,13 @@ class ClientClass extends MFWCommandPrototypeClass {
       URL: process.env.SELF_URL
     }
 
-    if(this.accesstoken) {
+    if (this.accesstoken) {
       clientSettings.accessToken = this.accesstoken;
     } else {
       if (process.env.SECURE_KEY) {
         clientSettings.secureKey = process.env.SECURE_KEY;
       } else {
-        this.message('error', 'You did not specify --accesstoken and there is no secure_key ' 
+        this.message('error', 'You did not specify --accesstoken and there is no secure_key '
           + module.short);
         return false;
       }
@@ -238,10 +238,10 @@ class ClientClass extends MFWCommandPrototypeClass {
   answerHandler(err, handlerResponse) {
     if (err) {
       let message = 'Unknown message: ' +  JSON.stringify(err);
-      if(err.message) {
+      if (err.message) {
         message = err.message
       }
-      if(err.error) {
+      if (err.error) {
         message = err.error
       }
       return this.message('error', message);
@@ -266,88 +266,88 @@ module.exports.commander = function(commander) {
       let settings = {
         RootDirectory: CommonFunc.getRoot(options)
       };
-      if(options.token) {
+      if (options.token) {
         settings.token =  options.token;
       }
-      if(options.accesstoken) {
+      if (options.accesstoken) {
         settings.accesstoken =  options.accesstoken;
       }
       var client = new ClientClass(settings);
       client.post(service, jsonData);
     });
-    commander.command('client-read <service> <id>')
-      .description('Read resource by ID from service.')
-      .option('-r, --root <dir>', 'Optionally root directory')
-      .option('-t, --token <token>', 'Optionally token.')
-      .option('-a, --accesstoken <accesstoken>', 'Optionally access token.')
-      .action(function(service, id, options) {
-        let settings = {
-          RootDirectory: CommonFunc.getRoot(options)
-        };
-        if(options.token) {
-          settings.token =  options.token;
-        }
-        if(options.accesstoken) {
-          settings.accesstoken =  options.accesstoken;
-        }
-        var client = new ClientClass(settings);
-        client.get(service, id);
+  commander.command('client-read <service> <id>')
+    .description('Read resource by ID from service.')
+    .option('-r, --root <dir>', 'Optionally root directory')
+    .option('-t, --token <token>', 'Optionally token.')
+    .option('-a, --accesstoken <accesstoken>', 'Optionally access token.')
+    .action(function(service, id, options) {
+      let settings = {
+        RootDirectory: CommonFunc.getRoot(options)
+      };
+      if (options.token) {
+        settings.token =  options.token;
+      }
+      if (options.accesstoken) {
+        settings.accesstoken =  options.accesstoken;
+      }
+      var client = new ClientClass(settings);
+      client.get(service, id);
     });
-    commander.command('client-update <service> <id> <JSONDATA>')
-      .description('Update resource by ID for service.')
-      .option('-r, --root <dir>', 'Optionally root directory')
-      .option('-t, --token <token>', 'Optionally token. If no token is provided, then SECURE_KEY used.')
-      .option('-a, --accesstoken <accesstoken>',
-        'Optionally access token. If no token is provided, then SECURE_KEY is used instead.')
-      .action(function(service, id, jsonData, options) {
-        let settings = {
-          RootDirectory: CommonFunc.getRoot(options)
-        };
-        if(options.token) {
-          settings.token =  options.token;
-        }
-        if(options.accesstoken) {
-          settings.accesstoken =  options.accesstoken;
-        }
-        var client = new ClientClass(settings);
-        client.put(service, id, jsonData);
+  commander.command('client-update <service> <id> <JSONDATA>')
+    .description('Update resource by ID for service.')
+    .option('-r, --root <dir>', 'Optionally root directory')
+    .option('-t, --token <token>', 'Optionally token. If no token is provided, then SECURE_KEY used.')
+    .option('-a, --accesstoken <accesstoken>',
+      'Optionally access token. If no token is provided, then SECURE_KEY is used instead.')
+    .action(function(service, id, jsonData, options) {
+      let settings = {
+        RootDirectory: CommonFunc.getRoot(options)
+      };
+      if (options.token) {
+        settings.token =  options.token;
+      }
+      if (options.accesstoken) {
+        settings.accesstoken =  options.accesstoken;
+      }
+      var client = new ClientClass(settings);
+      client.put(service, id, jsonData);
     });
-    commander.command('client-delete <service> <id>')
-      .description('Delete resource by ID from service.')
-      .option('-r, --root <dir>', 'Optionally root directory')
-      .option('-t, --token <token>', 'Optionally token.')
-      .option('-a, --accesstoken <accesstoken>', 'Optionaly access token.')
-      .action(function(service, id, options) {
-        let settings = {
-          RootDirectory: CommonFunc.getRoot(options)
-        };
-        if(options.token) {
-          settings.token =  options.token;
-        }
-        if(options.accesstoken) {
-          settings.accesstoken =  options.accesstoken;
-        }
-        var client = new ClientClass(settings);
-        client.delete(service, id);
+  commander.command('client-delete <service> <id>')
+    .description('Delete resource by ID from service.')
+    .option('-r, --root <dir>', 'Optionally root directory')
+    .option('-t, --token <token>', 'Optionally token.')
+    .option('-a, --accesstoken <accesstoken>', 'Optionaly access token.')
+    .action(function(service, id, options) {
+      let settings = {
+        RootDirectory: CommonFunc.getRoot(options)
+      };
+      if (options.token) {
+        settings.token =  options.token;
+      }
+      if (options.accesstoken) {
+        settings.accesstoken =  options.accesstoken;
+      }
+      var client = new ClientClass(settings);
+      client.delete(service, id);
     });
-    commander.command('client-search <service> <JSONDATA>')
-      .description('Search resource in service.')
-      .option('-r, --root <dir>', 'Optionally root directory')
-      .option('-t, --token <token>',
-        'Optionally token. If no is token provided, then SECURE_KEY is used instead.')
-      .option('-a, --accesstoken <accesstoken>',
-        'Optionally access token. If no token is provided, then SECURE_KEY is used instead.')
-      .action(function(service, jsonData, options) {
-        let settings = {
-          RootDirectory: CommonFunc.getRoot(options)
-        };
-        if(options.token) {
-          settings.token =  options.token;
-        }
-        if(options.accesstoken) {
-          settings.accesstoken =  options.accesstoken;
-        }
-        var client = new ClientClass(settings);
-        client.search(service, jsonData);
+  commander.command('client-search <service> <JSONDATA>')
+    .description('Search resource in service.')
+    .option('-r, --root <dir>', 'Optionally root directory')
+    .option('-t, --token <token>',
+      'Optionally token. If no is token provided, then SECURE_KEY is used instead.')
+    .option('-a, --accesstoken <accesstoken>',
+      'Optionally access token. If no token is provided, then SECURE_KEY is used instead.')
+    .action(function(service, jsonData, options) {
+      let settings = {
+        RootDirectory: CommonFunc.getRoot(options)
+      };
+      if (options.token) {
+        settings.token =  options.token;
+      }
+      if (options.accesstoken) {
+        settings.accesstoken =  options.accesstoken;
+      }
+      var client = new ClientClass(settings);
+      client.search(service, jsonData);
     });
 }
