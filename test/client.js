86c41b7f4e1d4504e651c29497449d0ef946fa82
Gor Martsen <Gor.martsen@gmail.com>
2018-03-04 22:04:48 -0500

    #11 cover left client tests
    - client-read
    - client-update
    - client-delete
    - client search

--- test/client.js
+++ test/client.js
@@ -75,21 +75,101 @@ describe('Client commands',function(){
     });
 
   });
-
+  var clientAnswer;
   it('client-create example-todo \'{ "title": "test"}\'', function(done){
     var rootDir = process.cwd();
 
     execMFW(rootDir + '/bin/mfw', [ "client-create", "example-todo",
       JSON.stringify({ title: "test"})],
       tmpCWD.name, (code, output) => {
-      console.log(output);
+      try {
+        clientAnswer = JSON.parse(output);
+      } catch (e) {
+        expect(e).to.equal(null, "JSON PARSE ERROR \n" + e + "\n" + output);
+      }
+      expect(code).to.equal(0, "client-create exited with code " + code + "\n" + output);
+      expect(clientAnswer.title).to.equal("test", "Title is not test \n" + output);
+      done();
+    });
+
+  });
+  it('client-read example-todo id', function(done){
+    var rootDir = process.cwd();
+
+    execMFW(rootDir + '/bin/mfw', [ "client-read", "example-todo",
+      clientAnswer.id ,
+      "-t" ,
+      clientAnswer.token],
+      tmpCWD.name, (code, output) => {
       try {
         var answer = JSON.parse(output);
       } catch (e) {
         expect(e).to.equal(null, "JSON PARSE ERROR \n" + e + "\n" + output);
       }
-      expect(code).to.equal(0, "client-create exited with code " + code + "\n" + output);
+      expect(code).to.equal(0, "client-read exited with code " + code + "\n" + output);
       expect(answer.title).to.equal("test", "Title is not test \n" + output);
+      done();
+    });
+
+  });
+
+  it('client-update example-todo id JSONDATA', function(done){
+    var rootDir = process.cwd();
+
+    execMFW(rootDir + '/bin/mfw', [ "client-update", "example-todo",
+      clientAnswer.id,
+      JSON.stringify({title: 'newtitle'}),
+      "-t" ,
+      clientAnswer.token],
+      tmpCWD.name, (code, output) => {
+      try {
+        var answer = JSON.parse(output);
+      } catch (e) {
+        expect(e).to.equal(null, "JSON PARSE ERROR \n" + e + "\n" + output);
+      }
+      expect(code).to.equal(0, "client-update exited with code " + code + "\n" + output);
+      expect(answer.title).to.equal("newtitle", "Title is not test \n" + output);
+      done();
+    });
+
+  });
+
+  it('client-search example-todo JSONDATA', function(done){
+    var rootDir = process.cwd();
+
+    execMFW(rootDir + '/bin/mfw', [ "client-search", "example-todo",
+      JSON.stringify({title: 'newtitle'}),
+      "-t" ,
+      clientAnswer.token],
+      tmpCWD.name, (code, output) => {
+      try {
+        var answer = JSON.parse(output);
+      } catch (e) {
+        expect(e).to.equal(null, "JSON PARSE ERROR \n" + e + "\n" + output);
+      }
+      expect(code).to.equal(0, "client-search exited with code " + code + "\n" + output);
+      expect(answer.length).to.equal(1, "Should be only one item\n" + output);
+      expect(answer[0].title).to.equal("newtitle", "Title is not test \n" + output);
+      done();
+    });
+
+  });
+
+  it('client-delete example-todo id', function(done){
+    var rootDir = process.cwd();
+
+    execMFW(rootDir + '/bin/mfw', [ "client-delete", "example-todo",
+      clientAnswer.id ,
+      "-t" ,
+      clientAnswer.token],
+      tmpCWD.name, (code, output) => {
+      try {
+        var answer = JSON.parse(output);
+      } catch (e) {
+        expect(e).to.equal(null, "JSON PARSE ERROR \n" + e + "\n" + output);
+      }
+      expect(code).to.equal(0, "client-delete exited with code " + code + "\n" + output);
+      expect(answer.title).to.equal("newtitle", "Title is not test \n" + output);
       done();
     });
 
@@ -107,4 +187,14 @@ describe('Client commands',function(){
     });
 
   });
+
+  it('Clean TMP dirs', function(done){
+    if (!debug){ 
+      fs.emptyDirSync(tmpCWD.name);
+      tmpCWD.removeCallback();
+    } else {
+      console.log('Dont forget to remove tmp dir: ' + tmpCWD.name + "\n");
+    }
+    done();
+  })
 });
